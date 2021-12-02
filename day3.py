acf345dee380520ef4180924f5d8dbf412196b8d
InvictusMalum <tylerjweed@gmail.com>
2021-12-02 13:20:47 -0500

    Day3,4

--- day3.py
+++ day3.py
@@ -1,2 +1,47 @@
 f = open("day3input.txt", "r")
 input = f.read()
+
+
+def move(x,y,moveCode):
+    if moveCode == "^":
+        y += 1
+    elif moveCode == ">":
+        x += 1
+    elif moveCode == "v":
+        y -= 1
+    elif moveCode == "<":
+        x -= 1
+    return x,y
+
+def countHousesMovedTo(instructionString):
+    x = 0
+    y = 0
+
+    positions = {(0,0):1}
+    for moveCode in instructionString:
+        x,y = move(x,y, moveCode)
+        positions[(x,y)] = 1
+    
+    return len(positions)
+
+def countHousesWithRobo(instructionString):
+    santaX = 0
+    santaY = 0
+
+    roboX = 0
+    roboY = 0
+
+    positions = {(0,0):1}
+    i = 0
+    for moveCode in instructionString:
+        if i%2 == 0:
+            santaX,santaY = move(santaX,santaY, moveCode)
+            positions[(santaX,santaY)] = 1
+        else:
+            roboX,roboY = move(roboX,roboY, moveCode)
+            positions[(roboX,roboY)] = 1
+        i+=1
+    return len(positions)
+
+print(countHousesMovedTo(input))
+print(countHousesWithRobo(input))
--- day4.py
+++ day4.py
@@ -1,2 +1,21 @@
 f = open("day4input.txt", "r")
 input = f.read()
+
+import hashlib
+
+def MD5HashOf(string):
+    return str(hashlib.md5(string.encode()))
+
+def StartWith5Zeroes(string):
+    if len(string) > 5 and string[0:5] == "00000":
+        return True
+    return False
+
+def findValidCode(letters):
+    number = 0
+    while True:
+        number += 1
+        if StartWith5Zeroes(MD5HashOf(letters+str(number))):
+            return number
+
+print(findValidCode(input))
