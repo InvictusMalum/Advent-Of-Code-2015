b098f9aa543ddcc1ee7289801c5e188d48214ccd
InvictusMalum <tylerjweed@gmail.com>
2021-12-07 12:59:31 -0500

    Day10

--- day10.py
+++ day10.py
@@ -1,2 +1,41 @@
 f = open("day10input.txt", "r")
 input = f.read()
+
+input = int(input)
+
+def fakeLookAndSay(number):
+    out = ''
+    
+    digitCounts = {}
+    number = str(number)
+    for char in number:
+        if char not in digitCounts:
+            digitCounts[char] = 1
+        else:
+            digitCounts[char] += 1
+    
+    for digit in digitCounts:
+        out += str(digitCounts[digit]) + digit
+    
+    return out
+
+def lookAndSay(number):
+    out = ''
+    number = str(number) + '-'
+    
+    count = 1
+    for i in range(len(number)-1):
+        if number[i] == number[i+1]:
+            count += 1
+        else:
+            out += str(count)+number[i]
+            count = 1
+    
+    return out
+
+applyNumber = 50
+for i in range(applyNumber):
+    input = lookAndSay(input)
+
+print(len(input))
+    
